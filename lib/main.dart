325fd85cbf86630e313c9acbc18c826c3e16d8c6
Selvi <Selvi@adani.com>
2023-03-20 12:47:49 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:learn_flutter_30days/pages/homePage.dart';
 import 'package:learn_flutter_30days/pages/loginPage.dart';
 
@@ -13,11 +14,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        //primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -6,7 +6,39 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(child: Center(child: Text("Login Page"))),
-    );
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/login.png",
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Welcome",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Username", hintText: "Enter Username"),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Password", hintText: "Enter Password"),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Login"))
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
