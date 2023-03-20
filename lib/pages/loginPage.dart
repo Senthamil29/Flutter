5e3d4062a943501dea6ed5cd3e3c9a63a8a6245b
Selvi <Selvi@adani.com>
2023-03-20 16:00:26 +0530

    Day 6 completed

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter_30days/utilis/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<StatefulWidget> createState() => MyLoginPage();
+}
+
+class MyLoginPage extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  String name = "";
+  bool buttonClicked = false;
+
   Widget build(BuildContext context) {
-    final formKey = GlobalKey<FormState>();
     return Material(
         child: SingleChildScrollView(
       child: Form(
@@ -21,7 +28,7 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Padding(
@@ -29,11 +36,15 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextFormField(
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                       decoration: InputDecoration(
                           labelText: "Username", hintText: "Enter Username"),
                       validator: (value) {
                         if (value!.isEmpty) {
-                          return "Enter Username";
+                          return "Username can't be empty";
                         }
                       }),
                   TextFormField(
@@ -42,20 +53,58 @@ class LoginPage extends StatelessWidget {
                         labelText: "Password", hintText: "Enter Password"),
                     validator: (value) {
                       if (value!.isEmpty) {
-                        return "Enter Password";
+                        return "Password can't be empty";
+                      } else if (value.length < 6) {
+                        return "Password length should be atleast 6 characters";
                       }
                     },
                   ),
                   SizedBox(
                     height: 20.0,
                   ),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          Navigator.pushNamed(context, MyRoute.homeRoute);
-                        }
-                      },
-                      child: Text("Login"))
+                  // ElevatedButton(
+                  //     onPressed: () {
+                  //       if (formKey.currentState!.validate()) {
+                  //         Navigator.pushNamed(context, MyRoute.homeRoute);
+                  //       }
+                  //     },
+                  //     child: Text("Login"))
+
+                  InkWell(
+                    onTap: () async {
+                      if (formKey.currentState!.validate()) {
+                        setState(() {
+                          buttonClicked = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoute.homeRoute);
+                      }
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: buttonClicked ? 40 : 130,
+                      height: 40,
+                      alignment: Alignment.center,
+                      child: buttonClicked
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(buttonClicked ? 50 : 8),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
--- lib/utilis/routes.dart
+++ lib/utilis/routes.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/src/widgets/framework.dart';
+
 class MyRoute {
   static String homeRoute = "/home";
   static String loginRoute = "/login";
