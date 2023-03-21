448441e171b1ea498b97dc1d140dbe800ca4e116
Selvi <Selvi@adani.com>
2023-03-21 11:28:56 +0530

    Day 8 completed

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_30days/models/catalog.dart';
 import 'package:learn_flutter_30days/widgets/drawer.dart';
+import 'package:learn_flutter_30days/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,16 +10,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     int days = 30;
     String name = "Flutter";
+    final dummyList = List.generate(10, (index) => CatalogModels.items[0]);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Flutter App",
+          "Catalog App",
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Container(
-          child: Center(child: Text("Welcome to $days days of $name"))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            //itemCount: CatalogModels.items.length,
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                //item: CatalogModels.items[index],
+                item: dummyList[index],
+              );
+            }),
+      ),
       drawer: MyDrawer(),
     );
   }
